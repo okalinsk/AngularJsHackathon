da510ff770e00efce54b0d0e8c1b53fb9faedeee
Dor Avraham <dor.a2003@gmail.com>
2014-12-28 23:30:19 +0200

    Added color index to the toilet view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,13 @@ angular.module('poogleApp')
   .controller('MainCtrl', function ($scope) {
     $scope.toilets = [
       { place : 'crown plasa hotel toilet', bestReview : "Best poo of my life" , upvotes: 4, downvotes: 1},
-      { place : 'Mashov toilets', bestReview : 'By far the toilet with the most exotic smell' , upvotes: 0, downvotes: 1000}
+      { place : 'Mashov toilets', bestReview : 'By far the toilet with the most exotic smell' , upvotes: 0, downvotes: 1000},
+      { place : 'Kneset israel', bestReview : 'Shit is all over the place', upvotes: 100, downvotes: 100 }
     ];
+    $scope.getStyle = function(toilet) {
+    	var downvotes = Math.max(1, toilet.downvotes);
+    	var upvotes = toilet.upvotes;
+    	var total = downvotes + upvotes
+    	return { color : "rgb(" + 255*(downvotes / total) + ", " + 255*(upvotes / total) + ", 0)" }
+    }
   });
