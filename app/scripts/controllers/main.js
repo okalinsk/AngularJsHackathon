a03288ce18db8be11eed678e1990c4690ff7403d
Dor Avraham <dor.a2003@gmail.com>
2014-12-28 23:46:51 +0200

    Fixed an issue with color not being viewed correctly
    also added two way data binding for the toilet voting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,14 +10,23 @@
 angular.module('poogleApp')
   .controller('MainCtrl', function ($scope) {
     $scope.toilets = [
-      { place : 'crown plasa hotel toilet', bestReview : "Best poo of my life" , upvotes: 4, downvotes: 1},
-      { place : 'Mashov toilets', bestReview : 'By far the toilet with the most exotic smell' , upvotes: 0, downvotes: 1000},
+      { place : 'crown plasa hotel toilet', bestReview : "Best poo of my life" , upvotes: 4, downvotes: 1 },
+      { place : 'Mashov toilets', bestReview : 'By far the toilet with the most exotic smell' , upvotes: 0, downvotes: 1000 },
       { place : 'Kneset israel', bestReview : 'Shit is all over the place', upvotes: 100, downvotes: 100 }
     ];
     $scope.getStyle = function(toilet) {
+    	console.log("Got style for " + toilet.place)
     	var downvotes = Math.max(1, toilet.downvotes);
     	var upvotes = toilet.upvotes;
-    	var total = downvotes + upvotes
-    	return { color : "rgb(" + 255*(downvotes / total) + ", " + 255*(upvotes / total) + ", 0)" }
+    	var total = downvotes + upvotes;
+    	return { color : "rgb(" + Math.floor(255*(downvotes / total)) + ", " + Math.floor(255*(upvotes / total)) + ", 0)" }
+    }
+    $scope.upVote = function (toilet) {
+    	toilet.upvotes += 1;
+    	toilet.toiletStyle = "getStyle(toilet)"
+    }
+    $scope.downVote = function (toilet) {
+    	toilet.downvotes += 1;
+    	toilet.toiletStyle = "getStyle(toilet)"
     }
   });
