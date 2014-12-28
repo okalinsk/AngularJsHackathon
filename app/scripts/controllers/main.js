4a423ecbccb3797b23d8b1ae7e70c9830ed3ac24
Dor Avraham <dor.a2003@gmail.com>
2014-12-28 23:00:29 +0200

    Changed the looks and feels, added upvotes and downvotes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,7 @@
 angular.module('poogleApp')
   .controller('MainCtrl', function ($scope) {
     $scope.toilets = [
-      { place : 'crown plasa hotel toilet', bestReview : "Best poo of my life" },
-      { place : 'Mashov toilets', bestReview : 'By far the toilet with the most exotic smell' }
+      { place : 'crown plasa hotel toilet', bestReview : "Best poo of my life" , upvotes: 4, downvotes: 1},
+      { place : 'Mashov toilets', bestReview : 'By far the toilet with the most exotic smell' , upvotes: 0, downvotes: 1000}
     ];
   });
